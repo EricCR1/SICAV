0ba9ada7bc7a3bba2496e9314ffc7fb38e36a667
EricC <al23760307@ite.edu.mx>
2025-11-28 00:01:01 -0800

    formularios responsivos

--- SimulacionLector/entrada_rfid.php
+++ SimulacionLector/entrada_rfid.php
@@ -13,8 +13,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo "✅ Entrada registrada correctamente.";
 
         // // ---------- Ejecutar Python para activar el LED ----------
-        // $command = "python arduino_led.py";
-        // exec($command);
+    $command = "python arduino_led.py";
+    exec($command);
 
     } catch (mysqli_sql_exception $e) {
 
