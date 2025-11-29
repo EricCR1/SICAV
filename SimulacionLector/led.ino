9f276e7acb71c18c6543ac1049caadaaf886ff22
EricC <al23760307@ite.edu.mx>
2025-11-28 19:58:48 -0800

    codigo para arduino

---
+++ SimulacionLector/led.ino
@@ -0,0 +1,29 @@
+// void setup() {
+//   Serial.begin(9600);
+
+//   pinMode(9, OUTPUT);
+//   pinMode(10, OUTPUT);
+//   pinMode(11, OUTPUT);
+// }
+
+// void loop() {
+//   if (Serial.available()) {
+//     String comando = Serial.readStringUntil('\n');
+
+//     if (comando == "ON") {
+//       digitalWrite(11, HIGH);
+
+//       delay(10000);
+
+//       digitalWrite(11, LOW);
+//     }
+//     else if(comando = "OFF")
+//     {
+//       digitalWrite(8, HIGH);
+
+//       delay(10000);
+
+//       digitalWrite(8, LOW);
+//     }
+//   }
+// }
